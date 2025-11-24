4a3d017231b2210a4b7d614030af7ba2247459e1
BetterLordWilliam <willjordan05@icloud.com>
2025-11-23 19:48:24 -0800

    WO: admin stats returned hitting /admin endpoint of this service.

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -7,8 +7,11 @@ router.use(express.json());
 router.route('/')
     .get((req, res) => {
         const as = req.services.adminService;
+
+        console.log(as.getApiStats());
         
-        res.send('gaming');
+        res.status(200);
+        res.json(as.getApiStats());
     });
 
 export { router as AdminRouter };
